f9f80266b8b31796b69d2a306b18dcba5aee53a7
Andrey Filipov <andrey@filipov.dev>
2020-04-13 20:46:58 +0300

    world_web_server-1 Parsing command line arguments and subcommands

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 #[macro_use]
+extern crate clap;
+#[macro_use]
 extern crate diesel;
 extern crate dotenv;
 extern crate serde;
 
 use actix_web::{web, App, HttpServer};
+use clap::App as ClapApp;
 
 use crate::logic::controllers::location;
 
@@ -15,13 +18,20 @@ const LOCATION_RADIUS: i32 = 10;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/index", web::get().to(location::index))
-            .route("/location", web::get().to(location::index))
-            .route("/location/generate", web::get().to(location::generate))
-    })
-    .bind("127.0.0.1:8088")?
-    .run()
-    .await
+    let yaml = load_yaml!("cli.yml");
+    let cmd = ClapApp::from_yaml(yaml).get_matches();
+
+    match cmd.subcommand().0 {
+        _ => {
+            HttpServer::new(|| {
+                App::new()
+                    .route("/index", web::get().to(location::index))
+                    .route("/location", web::get().to(location::index))
+                    .route("/location/generate", web::get().to(location::generate))
+            })
+                .bind("127.0.0.1:8088")?
+                .run()
+                .await
+        }
+    }
 }
